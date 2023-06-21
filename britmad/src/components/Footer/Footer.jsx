9223de8966813c2abba21d0814bbcd20302146c4
Javier Lobato <davidlobato99@gmail.com>
2023-06-21 15:57:34 -0500

    creations a modifications components footer

--- britmad/src/components/Footer/Footer.jsx
+++ britmad/src/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-import './footer.css'
-
-
+import "./footer.css";
 
 const Footer = () => {
     return (
-        <footer>CONTACTANOS</footer>
-    )
-}
+        <footer>
+            <div className="footer-contacto">
+                <h3>Contacto</h3>
+                <div className="footer_info">
+                    <div className="footer-email">
+                        <div className="circulo-naranja"></div>
+                        <p>Email: contacto@example.com</p>
+                    </div>
+                    <div className="footer-telefono">
+                        <div className="circulo-blanco"></div>
+                        <p>Teléfono: 123-456-7890</p>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-derechos">
+                <p>&copy; 2023 Todos los derechos reservados</p>
+            </div>
+        </footer>
+    );
+};
 
 export default Footer;
